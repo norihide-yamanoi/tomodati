c2617838d82f1acd08faaf2bf244079635748c25
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-08 18:09:57 +0900

    詳細画面に画像を表示できるよう設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -45,7 +45,7 @@ class PicturesController < ApplicationController
   private
 
   def picture_params
-    params.require(:picture).permit(:image, :content)
+    params.require(:picture).permit(:image, :image_cache, :content)
   end
 
   def set_picture
