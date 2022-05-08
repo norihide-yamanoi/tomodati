70252bb376c612f48dc020cac5007cb5d3b9202c
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-08 12:56:02 +0900

    投稿機能の修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,12 +8,13 @@ class PicturesController < ApplicationController
   end
 
   def create
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
+
     if @picture.save
-      redirect_to picture_path, notice: "作成しました！"
+      redirect_to pictures_path, notice: "作成しました！"
     else
       render :new
-    end  
+    end
   end
 
 def show
