14ad6a30bdb5082b2b1203ae95511927d92c7872
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-08 13:26:29 +0900

    パーシャル＆リファクタリング

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update]
+
   def index
     @pictures = Picture.all
   end
@@ -33,9 +35,16 @@ class PicturesController < ApplicationController
         render :edit
       end
   end
+
+
   private
 
   def picture_params
     params.require(:picture).permit(:image, :content)
   end
+
+  def set_picture
+  @picture = Picture.find(params[:id])
+end
+
 end
